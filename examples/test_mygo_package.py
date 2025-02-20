de5a93ec757652230ba778be38b538b930adf015
Ryan <60561289+qaz45647@users.noreply.github.com>
2025-02-20 20:54:53 +0800

    Update test_mygo_package.py

--- examples/test_mygo_package.py
+++ examples/test_mygo_package.py
@@ -9,3 +9,11 @@ print(response)
 print(f"Quote: {response['quote']}")
 print(f"Image URL: {response['image_url']}")
 
+
+# Get top 5 candidates
+result = chat.chat_with_candidates("想要那個 低頭求我啊")
+print(result)
+print('\n')
+#取出第二個結果
+print(f"Quote: {result['candidates'][1]['quote']}")
+print(f"Image URL: {result['candidates'][1]['image_url']}")
